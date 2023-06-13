62adc1834d66c919143f28d6e6581fdeb8b03ca1
yellowon0103 <gyw1teens@gmail.com>
2023-06-13 21:17:30 +0900

    ㅇㅇ

--- 0316test_vive/Assets/PunchFolder/ControllerUiClick.cs
+++ 0316test_vive/Assets/PunchFolder/ControllerUiClick.cs
@@ -1,36 +1,75 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class ControllerUiClick : MonoBehaviour
+{
+    public Transform crosshair;
+    public GraphicRaycaster raycaster;
+
+    void Update()
+    {
+        ARAVRInput.DrawCrosshair(crosshair);
+
+        if (ARAVRInput.Get(ARAVRInput.Button.One))
+        {
+            Debug.Log("Trigger!");
+
+            Ray ray = new Ray(ARAVRInput.LHandPosition, ARAVRInput.LHandDirection);
+            RaycastHit hitInfo;
+
+            // GraphicRaycaster를 사용하여 UI 요소와 충돌 감지
+            PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+            pointerEventData.position = new Vector2(Screen.width / 2f, Screen.height / 2f); // 화면 중앙으로 설정
+            List<RaycastResult> results = new List<RaycastResult>();
+            raycaster.Raycast(pointerEventData, results);
+
+            if (results.Count > 0)
+            {
+                Debug.Log("Hit!");
+            }
+        }
+    }
+}
+
+
+
+
+
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI; // 추가: Button을 사용하기 위한 네임스페이스
+using UnityEngine.UI;
 
 public class ControllerUiClick : MonoBehaviour
 {
-    // 크로스헤어 변수
     public Transform crosshair;
 
     void Update()
     {
-        // 크로스헤어 그리기
         ARAVRInput.DrawCrosshair(crosshair);
 
-        // 1) VR 컨트롤러의 발사 버튼을 누르면
         if (ARAVRInput.Get(ARAVRInput.Button.One))
         {
-            // 2) 컨트롤러가 향하는 방향으로 시선 만들기
+            Debug.Log("Trigger!");
+
             Ray ray = new Ray(ARAVRInput.LHandPosition, ARAVRInput.LHandDirection);
             RaycastHit hitInfo;
 
-            // 2. 마우스의 위치가 바닥 위에 위치해 있다면
             if (Physics.Raycast(ray, out hitInfo))
             {
-                // 3) Raycast로 검출된 객체가 UI 버튼인지 확인
+                Debug.Log("Raycast!");
                 if (hitInfo.collider.GetComponent<Button>())
                 {
-                    // 4) UI 버튼에 클릭 이벤트 전달
-                    ExecuteEvents.Execute(hitInfo.collider.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+                    Debug.Log("Button!");
+                    Button button = hitInfo.collider.GetComponent<Button>();
+                    button.onClick.Invoke();
                 }
             }
         }
     }
 }
+*/
