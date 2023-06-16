d5ec20b245aacadd9288dffca058d324197cffed
yellowon0103 <gyw1teens@gmail.com>
2023-06-16 17:05:39 +0900

    주먹수정완

--- 0316test_vive/Assets/PunchFolder/Bullet.cs
+++ 0316test_vive/Assets/PunchFolder/Bullet.cs
@@ -16,24 +16,18 @@ public class Bullet : MonoBehaviour
         bulletRigidbody.velocity = transform.forward * speed;
 
         // 3초 뒤에 자신의 게임 오브젝트 파괴
-        Destroy(gameObject, 3f);
+        //Destroy(gameObject, 3f);
     }
 
+    
     // 트리거 충돌 시 자동으로 실행되는 메서드
     void OnTriggerEnter(Collider other)
     {
         // 충돌한 상대방 게임 오브젝트가 Player 태그를 가진 경우
-        if (other.tag == "Player")
+        if (other.tag == "PunchWall")
         {
-            // 상대방 게임 오브젝트에서 PlayerController 컴포넌트를 가져오기
-            PlayerController playerController = other.GetComponent<PlayerController>();
-
-            // 상대방으로부터 PlayerController 컴포넌트를 가져오는데 성공했다면
-            if (playerController != null)
-            {
-                // 상대방 PlayerController 컴포넌트의 Die() 메서드 실행
-                playerController.Die();
-            }
+            //Debug.Log("Punch!");
+            Destroy(gameObject);
         }
     }
 
